f7c1aa68510fb2e5b208b5030d2168d3e271f6ed
Steve Jackson <stevejackson@Steves-MacBook-Pro-3.local>
2023-08-25 09:35:01 +0300

    Debug failure rollback

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'ttbackend-production-d6df.up.railway.app'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array("APP_KEYS"),
