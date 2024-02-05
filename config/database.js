8b001fd9480dd305ac091dabc37dc5406ae7d287
Steve Jackson <stevejackson@Steves-MacBook-Pro-3.local>
2024-02-05 15:33:56 +0200

    Modified DB Railways params

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
     connection: {
       client: 'postgres',
       connection: {
-        host: env('PGHOST', '127.0.0.1'),
-        port: env.int('PGPORT', 7870),
+        host: env('PGHOST', 'viaduct.proxy.rlwy.net'),
+        port: env.int('PGPORT', 59129),
         database: env('PGDATABASE', 'railway'),
         user: env('PGUSER', 'postgres'),
         password: env('PGPASSWORD', 'password'),
